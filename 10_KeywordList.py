fc2ea6e14e71c8b86f7916f245288904fe1370f0
mehar sulaiman <77274903+kali216@users.noreply.github.com>
2021-09-24 07:44:19 +0000

    Add files via upload

---
+++ 10_KeywordList.py
@@ -0,0 +1,3 @@
+#how to print the keywords or reserved words of python
+import keyword  #keyword is a library in python it can be imported by thi9s command 
+print(keyword.kwlist) #in keyword library there is a kw (k stand for key and w stand for word) module
---
+++ 11_InfoAboutVariable.py
@@ -0,0 +1,7 @@
+#many variables can define in one line as,
+a , b , c = 1 , 2 , 3
+#there are three names in left also in right otherwise error occur
+#to define variables values should be aqual in both sides if you havent take _ on the place of lease value for equalling both sides
+
+print(a , b , c) #you can print many variables as this  
+print(a) #you can also print them one by one
